4d0589511695d190e9e55cced1df4ecfb26a35b5
Elias Stefanidi <vcimka96@gmail.com>
2018-06-26 12:36:45 +0500

    *

--- app/.externalNativeBuild/cmake/release/armeabi/cmake_install.cmake
+++ app/.externalNativeBuild/cmake/release/armeabi/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/PaymonAndroid/app
+# Install script for directory: C:/Users/vcimk/Desktop/paymonandroid/app
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/PaymonAndroid/app/.externalNativeBuild/cmake/release/armeabi/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/vcimk/Desktop/paymonandroid/app/.externalNativeBuild/cmake/release/armeabi/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
