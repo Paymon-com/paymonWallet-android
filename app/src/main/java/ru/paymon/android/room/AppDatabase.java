b415a28ed87a6684d436b35564a3113a084ea3c1
Raingava <raingava@yandex.ru>
2018-10-03 16:58:15 +0500

    room groups and users

--- app/src/main/java/ru/paymon/android/room/AppDatabase.java
+++ app/src/main/java/ru/paymon/android/room/AppDatabase.java
@@ -6,8 +6,10 @@ import android.arch.persistence.room.RoomDatabase;
 import ru.paymon.android.models.ChatsItem;
 import ru.paymon.android.net.RPC;
 
-@Database(entities = {RPC.Message.class, ChatsItem.class}, version = 1)
+@Database(entities = {RPC.Message.class, ChatsItem.class, RPC.UserObject.class, RPC.Group.class}, version = 1)
 public abstract class AppDatabase extends RoomDatabase {
     public abstract ChatMessageDao chatMessageDao();
     public abstract ChatDao chatsDao();
+    public abstract UserDao userDao();
+    public abstract GroupDao groupDao();
 }
---
+++ app/src/main/java/ru/paymon/android/room/GroupDao.java
@@ -0,0 +1,30 @@
+package ru.paymon.android.room;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import java.util.List;
+
+import ru.paymon.android.net.RPC;
+
+@Dao
+public interface GroupDao {
+
+    @Query("SELECT * FROM `Group`")
+    List<RPC.Group> getAll();
+
+    @Query("SELECT * FROM `Group` WHERE id = :id")
+    RPC.Group getById(int id);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertList(List<RPC.Group> groups);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(RPC.Group group);
+
+    @Delete
+    void delete(RPC.Group group);
+}
