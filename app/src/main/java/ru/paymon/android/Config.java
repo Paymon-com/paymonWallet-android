969f4a2809eee55c12674523e50f46d094fee9d8
Elias Stefanidi <vcimka96@gmail.com>
2018-11-12 21:10:00 +0500

    fix

--- app/src/main/java/ru/paymon/android/Config.java
+++ app/src/main/java/ru/paymon/android/Config.java
@@ -6,9 +6,7 @@ import java.util.HashSet;
 import java.util.Locale;
 
 public class Config {
-    public static final boolean DEBUG = true;
-    public static final boolean PRODUCTION_VERSION = true;
-    public static final boolean TEST_BITCOIN = false;
+    public static final boolean DEBUG = false;
 
     public static String HOST;
     public static String HOST_ALT;
@@ -16,18 +14,11 @@ public class Config {
     public static final String TAG = "paymon-dbg";
     public static final String VERSION_STRING = "1.00";
 
-    //        public static short PORT = 7966;
     public static short PORT = 7968;
 
     static {
-//        if (DEBUG) {
-//            HOST = "192.168.88.168";
-//            HOST_ALT = "192.168.88.168";
-//            PORT = 7968;
-//        } else {
         HOST = "91.226.80.26";
         HOST_ALT = "91.226.80.26";
-//        }
     }
 
     public static final int VERSION = 0x1_08b;
