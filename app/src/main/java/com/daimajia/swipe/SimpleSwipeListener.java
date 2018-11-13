7aa9a8cf24be6ea702f8600dec0cfbf08739a83f
Elias Stefanidi <vcimka96@gmail.com>
2018-11-13 14:25:30 +0500

    fix

---
+++ app/src/main/java/com/daimajia/swipe/SimpleSwipeListener.java
@@ -0,0 +1,28 @@
+package com.daimajia.swipe;
+
+public class SimpleSwipeListener implements com.daimajia.swipe.SwipeLayout.SwipeListener {
+
+    @Override
+    public void onStartOpen(com.daimajia.swipe.SwipeLayout layout) {
+    }
+
+    @Override
+    public void onOpen(com.daimajia.swipe.SwipeLayout layout) {
+    }
+
+    @Override
+    public void onStartClose(com.daimajia.swipe.SwipeLayout layout) {
+    }
+
+    @Override
+    public void onClose(com.daimajia.swipe.SwipeLayout layout) {
+    }
+
+    @Override
+    public void onUpdate(com.daimajia.swipe.SwipeLayout layout, int leftOffset, int topOffset) {
+    }
+
+    @Override
+    public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
+    }
+}
