b1d80a54cac00464d2d3eb2c96a412068b8afe5a
Elias Stefanidi <vcimka96@gmail.com>
2018-11-14 13:20:24 +0500

    fix

--- app/src/main/java/ru/paymon/android/view/FragmentFriendProfile.java
+++ app/src/main/java/ru/paymon/android/view/FragmentFriendProfile.java
@@ -25,7 +25,7 @@ import static ru.paymon.android.view.FragmentChat.CHAT_ID_KEY;
 
 public class FragmentFriendProfile extends Fragment {
     private int userId;
-    private ImageButton floatMenu;
+    //    private ImageButton floatMenu;
     private ImageButton chatButton;
 //    private ImageButton blockButton;
 //    private ImageButton showBitcoin;
@@ -63,19 +63,19 @@ public class FragmentFriendProfile extends Fragment {
 //        showEthereum.setVisibility(View.GONE);
 //        showBitcoin.setVisibility(View.GONE);
 
-        floatMenu = (ImageButton) view.findViewById(R.id.friend_profile_menu_button);
+//        floatMenu = (ImageButton) view.findViewById(R.id.friend_profile_menu_button);
         TextView friendProfileCity = (TextView) view.findViewById(R.id.friend_profile_city_text_view);
         TextView friendProfileLogin = (TextView) view.findViewById(R.id.status_friend_profile_text_view);
         TextView friendProfileName = (TextView) view.findViewById(R.id.name_friend_profile_text_view);
         CircularImageView avatar = (CircularImageView) view.findViewById(R.id.friend_profile_avatar_image_view);
 
-        floatMenu.setOnClickListener(view1 -> {
-            if (!isFABOpen) {
-                showFABMenu();
-            } else {
-                closeFABMenu();
-            }
-        });
+//        floatMenu.setOnClickListener(view1 -> {
+//            if (!isFABOpen) {
+//                showFABMenu();
+//            } else {
+//                closeFABMenu();
+//            }
+//        });
 
         Utils.netQueue.postRunnable(() -> {
             ApplicationLoader.applicationHandler.post(dialogProgress::show);
@@ -154,23 +154,23 @@ public class FragmentFriendProfile extends Fragment {
         return view;
     }
 
-    private void showFABMenu() {
-        isFABOpen = true;
-        floatMenu.animate().rotation(180);
-        chatButton.animate().translationY(getResources().getDimension(R.dimen.standard_65));
+//    private void showFABMenu() {
+//        isFABOpen = true;
+//        floatMenu.animate().rotation(180);
+//        chatButton.animate().translationY(getResources().getDimension(R.dimen.standard_65));
 //        blockButton.animate().translationY(getResources().getDimension(R.dimen.standard_105));
 //        showBitcoin.animate().translationY(getResources().getDimension(R.dimen.standard_110));
 //        showEthereum.animate().translationY(getResources().getDimension(R.dimen.standard_160));
-    }
+//    }
 
-    private void closeFABMenu() {
-        isFABOpen = false;
-        floatMenu.animate().rotation(0);
-        chatButton.animate().translationY(0);
+//    private void closeFABMenu() {
+//        isFABOpen = false;
+//        floatMenu.animate().rotation(0);
+//        chatButton.animate().translationY(0);
 //        showBitcoin.animate().translationY(0);
 //        showEthereum.animate().translationY(0);
 //        blockButton.animate().translationY(0);
-    }
+//    }
 
     @Override
     public void onResume() {
